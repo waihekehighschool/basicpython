4f14fddcd3fc78e736de9d86308a447ad49b3a60
brent <pumicehead@gmail.com>
2017-07-08 10:30:53 +1200

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,5 @@
+# run this script like this >> python userinput.py
+# it should ask for some input from the user before writing the input to the screen.
+
+name = raw_input('What is your name?\n')
+print 'Hi, %s.' % name
